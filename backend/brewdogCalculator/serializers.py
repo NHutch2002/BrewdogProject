6da3168591f8d2deb91a3a70c6cb862628412d9d
Ahmed Badawy <2613099b@student.gla.ac.uk>
2022-11-16 12:15:30 +0000

    setup backend lets gooooo

--- backend/brewdogCalculator/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import brewdogCalculator
-
-class brewdogCalculatorSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = brewdogCalculator
-        fields = ('breweryName', 'userName')
