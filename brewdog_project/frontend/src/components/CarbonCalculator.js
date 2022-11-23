8082f6fa51eebc3cab46041063c0da72b11f4050
Ahmed Badawy <2613099b@student.gla.ac.uk>
2022-11-23 13:43:17 +0000

    half functioning multiple renders

---
+++ brewdog_project/frontend/src/components/CarbonCalculator.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class CarbonCalculator extends Component {
+    constructor(props) {
+        super(props);
+    }
+    
+    render() {
+        return <p>This is the carbon calculator page</p>;
+    }
+}
