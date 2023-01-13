a8ff9bc659bb396d6c1be1d000fb5908575ac25b
GoldenZs3 <jakyzsofia@gmail.com>
2023-01-13 14:17:24 +0000

    feature
    issue #46 home page design added

--- brewdog_project/frontend/src/components/HomePage.js
+++ brewdog_project/frontend/src/components/HomePage.js
@@ -15,24 +15,30 @@ export default class HomePage extends Component {
         return (
             //USING INLINE CSS, NOT IDEAL, FIX THIS ASAP!! (note to self)
             //FOR THE FONT WE NEED TO FIX CSS IMPORTS
-            <div>
-                <img src={Banner} alt="Brewdog Banner" style={{align:"center", width:"100%"}}/>
-                <h1 align="center" >Planet First Locals</h1>
-                <p style={{align:"left", font:"ariel"}}>This programme is designed to support our clients
-                 in cutting their carbon, by understanding their 
-                 business emissions, including their supply chains, 
-                 and setting and achieving ambitious carbon reduction 
-                 targets.<br></br><br></br>
-                 Our aim is to build a network of dynamic and 
-                proactive bar and restaurant innovators that are 
-                committed to going as low carbon as they can, and 
-                then some.<br></br><br></br>
-                Real innovation happens when individuals get 
-                inspired to look beyond business as usual and 
-                commit to doing things differently. Here at BrewDog, 
-                we have totally embraced the need to be a business that 
-                is future-ready and supportive of those who are ready to 
-                join us.</p>
+            <div className="container-fluid bodycontent"  >
+                <div className="row">
+                    <div className="picDiv">
+                        <img className="pic" src={Banner} alt="Brewdog Banner"/>
+                    </div>
+                        <h1 className="title" >Planet First Locals</h1>
+                    <div className="textDiv">
+                        <p className="text">This programme is designed to support our clients
+                        in cutting their carbon, by understanding their 
+                        business emissions, including their supply chains, 
+                        and setting and achieving ambitious carbon reduction 
+                        targets.<br></br><br></br>
+                        Our aim is to build a network of dynamic and 
+                        proactive bar and restaurant innovators that are 
+                        committed to going as low carbon as they can, and 
+                        then some.<br></br><br></br>
+                        Real innovation happens when individuals get 
+                        inspired to look beyond business as usual and 
+                        commit to doing things differently. Here at BrewDog, 
+                        we have totally embraced the need to be a business that 
+                        is future-ready and supportive of those who are ready to 
+                        join us.</p>
+                        </div>
+                </div>
             </div>
         );   
     }
