1060643a7d0c22297d5cbe90fdc65c27bf661ca9
Ahmed Badawy <2613099b@student.gla.ac.uk>
2023-01-17 16:20:48 +0000

    Fix
    Login page now works and no longer crashes when open

--- brewdog_project/frontend/src/components/Login.js
+++ brewdog_project/frontend/src/components/Login.js
@@ -1,55 +1,23 @@
 import React, { Component } from 'react';
 import { Menu, Container } from '@material-ui/core';
 
-export class Login extends Component {
-    state = {
-        username: '',
-        password: ''
-    }
-
-    onChange = e => this.setState({ [e.target.name]: e.target.value });
-
-    onSubmit = e => {
-        e.preventDefault();
-        this.props.login(this.state.username, this.state.password);
-    }
+export default class Login extends Component {
 
     render() {
         return (
             <div>
-                <div className={this.style.toolbar} />
-                {/* <Menu>
-                    <Container>
-                        <h1>Login</h1>
-                    </Container>
-                </Menu> */}
-                <Container>
-                    <form onSubmit={this.onSubmit}>
-                        <div>
-                            <label>Username</label>
-                            <input
-                                type='text'
-                                name='username'
-                                onChange={this.onChange}
-                                value={this.state.username}
-                            />
-                        </div>
-                        <div>
-                            <label>Password</label>
-                            <input
-                                type='password'
-                                name='password'
-                                onChange={this.onChange}
-                                value={this.state.password}
-                            />
-                        </div>
-                        <div>
-                            <input type='submit' value='Login' />
-                        </div>
-                    </form>
-                </Container>
+                <h1>Login</h1>
+                <form method="POST" credentials="include">
+                    <input type="hidden" name="csrfmiddlewaretoken" value="csrftoken"/>
+                    <div>
+                        <label>Username:</label>
+                        <input type="text" name="username"/><br/>
+                        <label>Password:</label>
+                        <input type="password" name="password"/><br/>
+                        <input type="submit" value="Login"/>
+                    </div>
+                </form>
             </div>
         )
     }
 }
-export default Login;
