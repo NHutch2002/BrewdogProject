ce41c340f8e0a3c94491f70e6f182576f3a5afe1
2574266s <2574266s@student.gla.ac.uk>
2023-02-01 20:30:22 +0000

    Feature: landing and initial content added, closes issue #61

--- brewdog_project/frontend/src/components/HomePage.js
+++ brewdog_project/frontend/src/components/HomePage.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import Banner from "../../static/images/brewdog_banner.png";
-import "../../static/css/home.css";
+import Banner from "../../static/images/tree.jpeg";
+import FoodAtPub from "../../static/images/food_at_pub.png";
 import "../../static/css/base.css";
+import "../../static/css/home.css";
 
 export default class HomePage extends Component { 
     constructor(props) {
@@ -10,32 +11,37 @@ export default class HomePage extends Component {
 
     render() {
         return (
-            //USING INLINE CSS, NOT IDEAL, FIX THIS ASAP!! (note to self)
-            //FOR THE FONT WE NEED TO FIX CSS IMPORTS
-            <div className="container-fluid bodycontent"  >
-                <div className="row">
-                    <div className="picDiv">
-                        <img className="pic" src={Banner} alt="Brewdog Banner"/>
+            <div className="container-fluid" >
+                    
+                <div className="landing_pic_container">
+                    <img className="landing_pic" src={Banner} alt="Brewdog Banner"/>
+                    <div className="landing_title_container">
+                        <h1 className="landing_heading" >Planet First<br />Locals</h1>
+                        <h2 className="landing_subheading"><a id="landing_blue_accent">How low can you go?</a></h2>
                     </div>
-                        <h1 className="title" >Planet First Locals</h1>
-                    <div className="textDiv">
-                        <p className="text">This programme is designed to support our clients
-                        in cutting their carbon, by understanding their 
-                        business emissions, including their supply chains, 
-                        and setting and achieving ambitious carbon reduction 
-                        targets.<br></br><br></br>
-                        Our aim is to build a network of dynamic and 
-                        proactive bar and restaurant innovators that are 
-                        committed to going as low carbon as they can, and 
-                        then some.<br></br><br></br>
-                        Real innovation happens when individuals get 
-                        inspired to look beyond business as usual and 
-                        commit to doing things differently. Here at BrewDog, 
-                        we have totally embraced the need to be a business that 
-                        is future-ready and supportive of those who are ready to 
-                        join us.</p>
-                        </div>
                 </div>
+                
+                <div className="text_div">
+                    <h2 className="text_heading">Heading</h2>
+                    <p className="text">This programme supports our clients to cut
+                    in cutting their carbon, by understanding their business emissions, including 
+                    their supply chains, and setting and achieving ambitious carbon reduction 
+                    targets.<br></br><br></br>
+                    Our aim is to build a network of dynamic and 
+                    proactive bar and restaurant innovators that are 
+                    committed to going as low carbon as they can, and 
+                    then some.<br></br><br></br>
+                    Real innovation happens when individuals get 
+                    inspired to look beyond business as usual and 
+                    commit to doing things differently. Here at BrewDog, 
+                    we have totally embraced the need to be a business that 
+                    is future-ready and supportive of those who are ready to 
+                    join us.</p>
+                    <img className="text_pic" id="pic_food_at_pub" src={FoodAtPub} alt="Picture of some food at a Brewdog pub"/>
+                </div>
+
+                <hr />
+                
             </div>
         );   
     }
