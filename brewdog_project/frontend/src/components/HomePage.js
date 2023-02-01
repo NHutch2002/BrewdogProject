8012ad2540bc806bcd1077fad8fea3dea6917d21
2574266s <2574266s@student.gla.ac.uk>
2023-02-01 21:34:35 +0000

    Feature: finished main page content layout with dummy text, finishes issue #61

--- brewdog_project/frontend/src/components/HomePage.js
+++ brewdog_project/frontend/src/components/HomePage.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import Banner from "../../static/images/tree.jpeg";
+import Landing from "../../static/images/tree.jpeg";
 import FoodAtPub from "../../static/images/food_at_pub.png";
+import Calculator from "../../static/images/calculator_image.jpg";
 import "../../static/css/base.css";
 import "../../static/css/home.css";
 
@@ -14,7 +15,7 @@ export default class HomePage extends Component {
             <div className="container-fluid" >
                     
                 <div className="landing_pic_container">
-                    <img className="landing_pic" src={Banner} alt="Brewdog Banner"/>
+                    <img className="landing_pic" src={Landing} alt="Brewdog Banner"/>
                     <div className="landing_title_container">
                         <h1 className="landing_heading" >Planet First<br />Locals</h1>
                         <h2 className="landing_subheading"><a id="landing_blue_accent">How low can you go?</a></h2>
@@ -22,26 +23,57 @@ export default class HomePage extends Component {
                 </div>
                 
                 <div className="text_div">
-                    <h2 className="text_heading">Heading</h2>
-                    <p className="text">This programme supports our clients to cut
-                    in cutting their carbon, by understanding their business emissions, including 
-                    their supply chains, and setting and achieving ambitious carbon reduction 
-                    targets.<br></br><br></br>
-                    Our aim is to build a network of dynamic and 
-                    proactive bar and restaurant innovators that are 
-                    committed to going as low carbon as they can, and 
-                    then some.<br></br><br></br>
-                    Real innovation happens when individuals get 
-                    inspired to look beyond business as usual and 
-                    commit to doing things differently. Here at BrewDog, 
-                    we have totally embraced the need to be a business that 
-                    is future-ready and supportive of those who are ready to 
-                    join us.</p>
+                    <div id="intro_text">
+                        <h2 className="text_heading">Introduction</h2>
+                        <p className="text">This programme supports our clients to cut
+                        in cutting their carbon, by understanding their business emissions, including 
+                        their supply chains, and setting and achieving ambitious carbon reduction 
+                        targets.<br></br><br></br>
+                        Our aim is to build a network of dynamic and 
+                        proactive bar and restaurant innovators that are 
+                        committed to going as low carbon as they can, and 
+                        then some.<br></br><br></br>
+                        Real innovation happens when individuals get 
+                        inspired to look beyond business as usual and 
+                        commit to doing things differently. Here at BrewDog, 
+                        we have totally embraced the need to be a business that 
+                        is future-ready and supportive of those who are ready to 
+                        join us.</p>
+                    </div>
                     <img className="text_pic" id="pic_food_at_pub" src={FoodAtPub} alt="Picture of some food at a Brewdog pub"/>
                 </div>
 
-                <hr />
-                
+                <div className="text_div">
+                    <div id="other_text">
+                        <h2 className="text_heading">Other Heading</h2>
+                        <p className="text">Animi et voluptatem qui sed dolores velit aperiam et. Laborum est corrupti aut sit excepturi qui modi. Illo qui illum ut molestiae quam ut. In dolore esse tempore et possimus vitae ea. Assumenda similique quaerat velit. Ipsam aut laudantium ullam quis enim quae dolorem placeat.</p>
+                    </div>
+                    <img className="text_pic" id="pic_other" src={FoodAtPub} alt="Picture of some food at a Brewdog pub"/>
+                </div>
+
+                <div className="calculator_div">
+                    <div id="calculator_text">
+                        <h2 className="text_heading">Footprint Calculator</h2>
+                        <p className="text">This programme supports our clients to cut
+                        in cutting their carbon, by understanding their business emissions, including 
+                        their supply chains, and setting and achieving ambitious carbon reduction 
+                        targets.<br></br><br></br>
+                        Our aim is to build a network of dynamic and 
+                        proactive bar and restaurant innovators that are 
+                        committed to going as low carbon as they can, and 
+                        then some.<br></br><br></br>
+                        Real innovation happens when individuals get 
+                        inspired to look beyond business as usual and 
+                        commit to doing things differently. Here at BrewDog, 
+                        we have totally embraced the need to be a business that 
+                        is future-ready and supportive of those who are ready to 
+                        join us.</p>
+                        <button>&#9654; Launch</button>
+                    </div>
+                    <img className="text_pic" id="pic_calculator" src={Calculator} alt="Picture of a person using a calculator"/>
+                </div>
+
+
             </div>
         );   
     }
