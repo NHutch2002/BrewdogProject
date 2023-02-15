151e4291a5734e0db815f4e01a359cba3b4b56ed
2556961h <2556961h@student.gla.ac.uk>
2023-02-15 14:18:34 +0000

    Refactor: fixed dependency clashes to resolve pipeline failure, closes issue #75

--- brewdog_project/brewdog_api/tests.py
+++ brewdog_project/brewdog_api/tests.py
@@ -6,30 +6,6 @@ from django.core.exceptions import ValidationError
 
 # Create your tests here.
 
-class UserTests(TestCase):
-
-    #Tests if a user's account is successfully created with the appropriate fields.
-    def test_user_account_created(self):
-        User1 = User(name='George', company='Brewdog', email='abc@gmail.com', phone='111', password='f72Nei!cr?Di3d')
-        User1.save()
-        self.assertEqual((User1.name == 'George'), True)
-        self.assertEqual((User1.company == 'Brewdog'), True)
-        self.assertEqual((User1.email == 'abc@gmail.com'), True)
-        self.assertEqual((User1.phone == '111'), True)
-        self.assertEqual((User1.password == 'f72Nei!cr?Di3d'), True)
-
-    # Tests if a validation error is raised if the user has not typed in an email address.
-    def test_empty_email_not_allowed(self):
-        User2 = User(name='John', company='Brewdog', phone='111', password='f72Nei!cr?Di3d')
-        User2.save()
-        self.assertRaises(ValidationError, User2.full_clean)
-
-    # Tests if a company name cannot be used more than once.
-    def test_unique_company(self):
-        User3 = User(name='John', company='Brewdog', email='abc@gmail.com', phone='111', password='f72Nei!cr?Di3d')
-        User3.save()
-        User4 = User(name='David', company='Brewdog', email='def@gmail.com', phone='222', password='g72Nei!cr?Di3d')
-        self.assertRaises(ValidationError, User4.full_clean)
 
 #class NavigationTests(TestCase):
 
