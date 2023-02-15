9561d54837eb76a069548f53e00e94222a858f9a
2556961h <2556961h@student.gla.ac.uk>
2023-02-15 16:20:39 +0000

    Fix: reimplemented CSS styling for login page, closes issue #79

--- brewdog_project/frontend/src/components/Login.js
+++ brewdog_project/frontend/src/components/Login.js
@@ -38,27 +38,24 @@ const Login = () => {
     };
 
     return (
-        <form method="POST" credentials="include" onSubmit={handleSubmit}>
-        <input type="hidden" name="csrfmiddlewaretoken" value="csrftoken"/>
-            <label>
-                Email:
-                <input
-                    type="email"
-                    value={email}
-                    onChange={e => setEmail(e.target.value)}
-                />
-            </label>
-            <br />
-            <label>
-                Password:
-                <input
-                    type="password"
-                    value={password}
-                    onChange={e => setPassword(e.target.value)}
-                />
-            </label>
-            <br />
-            <button type="submit">Login</button>
+        <form class="account_form" method="POST" credentials="include" onSubmit={handleSubmit}>
+            <input type="hidden" name="csrfmiddlewaretoken" value="csrftoken"/>
+            <h2>Log In</h2>
+
+            <div class="form-outline mb-4 field_container">
+                <label className="form-label form-input-label" htmlFor="login-form-email">Email Address</label>
+                <input type="email" id="login-form-email" className="form-control form-input-field" />
+            </div>
+
+            <div className="form-outline mb-4 field_container">
+                <label className="form-label form-input-label" htmlFor="login-form-password">Password</label>
+                <input type="password" id="login-form-password" className="form-control form-input-field" />
+            </div>
+
+            <p>Don't have an account?<br/>Create one <a href="/signup">here</a>.</p>
+
+            <button type="submit" className="btn btn-primary btn-block">Submit</button>
+
         </form>
     );
 }
