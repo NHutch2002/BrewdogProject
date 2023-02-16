f8acb14f67bcd2fce709cf910f2c281ff62da664
2556961h <2556961h@student.gla.ac.uk>
2023-02-16 22:28:11 +0000

    Fix: restored login functionality lost in page redesign

--- brewdog_project/frontend/src/components/Login.js
+++ brewdog_project/frontend/src/components/Login.js
@@ -46,12 +46,12 @@ const Login = () => {
 
                     <div class="form-outline mb-4 field_container">
                         <label className="form-label form-input-label" htmlFor="login-form-email">Email Address</label>
-                        <input type="email" id="login-form-email" className="form-control form-input-field" />
+                        <input type="email" id="login-form-email" value={email} onChange={e => setEmail(e.target.value)}className="form-control form-input-field" />
                     </div>
 
                     <div className="form-outline mb-4 field_container">
                         <label className="form-label form-input-label" htmlFor="login-form-password">Password</label>
-                        <input type="password" id="login-form-password" className="form-control form-input-field" />
+                        <input type="password" id="login-form-password" value={password} onChange={e => setPassword(e.target.value)} className="form-control form-input-field" />
                     </div>
 
 
