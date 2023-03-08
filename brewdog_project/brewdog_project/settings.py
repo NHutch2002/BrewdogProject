8d0a7b8894fed3f59714eac4bac8a19f62c2ae09
2556961h <2556961h@student.gla.ac.uk>
2023-03-08 16:45:49 +0000

    Refactor: changed database from dbsqlite to postgresql to help with hosting, working on issue #87

--- brewdog_project/brewdog_project/settings.py
+++ brewdog_project/brewdog_project/settings.py
@@ -88,8 +88,12 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "brewdog",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
