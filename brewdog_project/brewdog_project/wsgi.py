1446f53e60b18282c2b7dc8e40885c5cbbbe1bd1
2556961h <2556961h@student.gla.ac.uk>
2023-03-15 17:44:06 +0000

    Refactor: reverting wsgi.py to help with hosting

--- brewdog_project/brewdog_project/wsgi.py
+++ brewdog_project/brewdog_project/wsgi.py
@@ -11,8 +11,12 @@ import os
 
 from whitenoise import WhiteNoise
 
+from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "brewdog_project.settings")
 
+application = get_wsgi_application()
+
 application = WhiteNoise(application, root='staticfiles')
+
 
